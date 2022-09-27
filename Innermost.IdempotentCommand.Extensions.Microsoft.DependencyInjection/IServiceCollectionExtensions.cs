9753e32cc1900b62b4c24f975b0b0eb699dd53c9
HongJieSunnn <457406475@qq.com>
2022-09-27 21:44:36 +0800

    Update IServiceCollectionExtensions.AddIdempotentCommandRequestSQLStorage
    completed.

--- Innermost.IdempotentCommand.Extensions.Microsoft.DependencyInjection/IServiceCollectionExtensions.cs
+++ Innermost.IdempotentCommand.Extensions.Microsoft.DependencyInjection/IServiceCollectionExtensions.cs
@@ -8,13 +8,8 @@ namespace Innermost.IdempotentCommand.Extensions.Microsoft.DependencyInjection
 {
     public static class IServiceCollectionExtensions
     {
-        private static bool _added = false;
         public static IServiceCollection AddIdempotentCommandRequestStorage(this IServiceCollection services)
         {
-            if (_added)
-                throw new InvalidOperationException("IdempotentCommandRequestSQLStorage has already been added.");
-            _added=true;
-
             services.AddSingleton<ICommandRequestRepository, CommandRequestRepository>(s =>
             {
                 var context = s.GetRequiredService<MongoDBContextBase>();
@@ -31,19 +26,7 @@ namespace Innermost.IdempotentCommand.Extensions.Microsoft.DependencyInjection
         public static IServiceCollection AddIdempotentCommandRequestSQLStorage<TDbContext>(this IServiceCollection services)
             where TDbContext:DbContext
         {
-            if (_added)
-                throw new InvalidOperationException("IdempotentCommandRequestStorage has already been added.");
-            _added = true;
-
-            services.AddSingleton<ICommandRequestRepository, CommandRequestRepository>(s =>
-            {
-                var context = s.GetRequiredService<MongoDBContextBase>();
-                var database = context.Database;
-
-                var session = s.GetRequiredService<IClientSessionHandle>();
-
-                return new CommandRequestRepository(session, database);
-            });
+            services.AddScoped<ICommandRequestRepository, CommandRequestSQLRepository<TDbContext>>();
 
             return services;
         }
